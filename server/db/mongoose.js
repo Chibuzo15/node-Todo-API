77f3f14d27d5d3a5229d7483e3deee8017e3b870
Chibuzo15 <Anyachris85@gmail.com>
2020-04-25 16:47:58 +0100

    heroku fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_qq786tmc:rudig2gcbe6ru8hbtmau2vb99k@ds057244.mlab.com:57244/heroku_qq786tmc' || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {
     mongoose
